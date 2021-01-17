f5d9d3b59e4aa10f5ebf3c4f3a1eaec9ab8da722
Manorath Naphaphone <Naphaphonem@outlook.com>
2021-01-16 18:06:21 -0800

    jan16

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,49 @@
-var timerEl = document.getElementById("time");
-var startbtn = document.getElementById("startbtn");
-var scoreEl = document.getElementById("score");
-var quizEl = document.getElementById("quiz");
+var timerEl = document.querySelector("#time");
+var startbtn = document.querySelector("#startbtn");
+var scoreEl = document.querySelector("#score");
+var quizEl = document.querySelector("#quiz");
 
+var questions = [
+  {
+    q: "What is git?",
+    c: ["a. Version Control", "b. Git is short for GitHub", "c. Slang for Get", "d. a programming language"],
+    a: "a. Version Control",
+  },
+  {
+    q: "Which element is considered the “root” HTML element?",
+    c: ["a. <head>", "b. <body>", "c. <!docTYPE html>", "d. <html>"],
+    a: "d. <html>",
+  },
+  {
+    q: "Which operator is used to assign a value to a variable??",
+    c: ["a. =", "b. -", "c. *", "d. X"],
+    a: "a. =",
+  },
+  {
+    q: "Javascript and Java are the same.",
+    c: ["a. True", "b. False"],
+    a: "b. False",
+  },
+  {
+    q: "Is JavaScript case-sensitive? git?",
+    c: ["a. N", "b. Yes"],
+    a: "b. Yes",
+  },
+];
 
-startbtn.addEventListener("click", function(){
+function start(){
+  startbtn.remove();
+  welcome.remove();
+};
+
+function getQuestion(){
+  for (var i = 0; i < questions.length; i++){
+    var question = this.q;
+    document.querySelector("#quiz").innerHTML = question;
+  }
+}
+
+function timer(){
 
   var time = 90;
 
@@ -15,8 +54,16 @@ startbtn.addEventListener("click", function(){
 
   if(time <= 0){
       clearInterval(countDownTimer);
-      document.getElementById("time").innerHTML = "Time is up!"
+      alert("Time is up!");
+      // document.getElementById("time").innerHTML = "Time is up!"
   }
   }, 1000);
-});
+};
   
+startbtn.addEventListener("click", function(){
+
+  start();
+  timer();
+  getQuestion();
+
+})
